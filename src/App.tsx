49123f20c2569063e8023df87b92d7a7d79aa70e
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-08-31 13:42:40 -0600

    fix: update language attribute to Spanish and improve talwind styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1> Initial </h1>
+      <h1
+        className="flex justify-center text-3xl font-bold  mt-8"
+        style={{ letterSpacing: "2px" }}
+      >
+        Initial
+      </h1>
     </>
   );
 }
