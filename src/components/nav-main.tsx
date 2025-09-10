f916a0beb4a08900530a5f666730b4ce3775ae32
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-10 13:25:22 -0600

    fix: update NavMain and Sidebar components for improved styling and responsiveness

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -6,6 +6,7 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { Link, useLocation } from "react-router";
@@ -25,6 +26,7 @@ export function NavMain({
   }[];
 }) {
   const pathname = useLocation().pathname;
+  const { open } = useSidebar();
 
   return (
     <SidebarGroup>
@@ -43,15 +45,23 @@ export function NavMain({
                 <Link to={item.url} className="w-full">
                   <SidebarMenuButton
                     className={cn(
+                      "",
                       "p-[22px] flex items-center gap-[10px] font-medium text-[14px] text-[#475569]",
                       isActive &&
                         "bg-slate-100 text-[#73C7E3] dark:bg-slate-800 dark:text-slate-100",
-                      "hover:text-none"
+                      "hover:text-none",
+                      !open && "justify-center"
                     )}
                     tooltip={item.title}
                   >
-                    {item.icon && <item.icon className="!w-6 !h-6" />}
-                    <span className="hover:text-inherit">{item.title}</span>
+                    {item.icon && (
+                      <item.icon className="!w-[19.77px] !h-[19.77px]" />
+                    )}
+                    <span
+                      className={cn("hover:text-inherit", !open && "hidden")}
+                    >
+                      {item.title}
+                    </span>
                   </SidebarMenuButton>
                 </Link>
               </SidebarMenuItem>
