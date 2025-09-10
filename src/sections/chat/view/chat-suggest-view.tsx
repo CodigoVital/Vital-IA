f48eddd0079294f70e652662ead877bb3b36f370
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-09 19:44:24 -0600

    feat: add ChatSuggestView component with suggestion buttons; create mock data for suggestions fix: update SidebarTrigger to use PanelLeftClose icon for better UX

---
+++ src/sections/chat/view/chat-suggest-view.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { suggestions } from "./data/suggest.mock";
+
+
+const ChatSuggestView = () => {
+  return (
+    <>
+      <section className=" text-black p-4 border-t border-t-muted/20 flex flex-col  gap-4">
+        <h2>O prueba una de estas opciones:</h2>
+        <div className="flex flex-row gap-2 flex-wrap mx-3">
+          {suggestions.map((suggestion, index) => (
+            <Button key={index} className="rounded-xl" variant="outline">
+              {suggestion}
+            </Button>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ChatSuggestView;
