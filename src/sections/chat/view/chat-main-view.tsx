c1d8514bddc42e0023541e479e2c7e85faad5535
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-09 18:18:06 -0600

    feat: implement ChatMessage component with avatar and message display; add sample messages for ChatWindowView

--- src/sections/chat/view/chat-main-view.tsx
+++ src/sections/chat/view/chat-main-view.tsx
@@ -1,5 +1,6 @@
 import { ChatHeader } from "../components/chat-header";
 import ChatInput from "../components/chat-input";
+import { ChatWindowView } from "./chat-window-view";
 
 export const ChatMainView = () => {
   return (
@@ -9,7 +10,7 @@ export const ChatMainView = () => {
       </div>
 
       <div className="flex-1 overflow-y-auto p-4 ">
-
+        <ChatWindowView />
       </div>
 
       <div className="shrink-0  ">
--- src/sections/chat/view/chat-window-view.tsx
+++ src/sections/chat/view/chat-window-view.tsx
@@ -1,3 +1,20 @@
+import ChatMessage from "../components/chat-message";
+import { sampleMessages } from "./data/message.mock";
+
 export const ChatWindowView = () => {
-  return <div>ChatWindowView</div>;
+  return (
+    <div className=" overflow-auto flex flex-col bg-background">
+      <div className="flex-1 overflow-y-auto p-4 space-y-1">
+        {sampleMessages.map((msg) => (
+          <ChatMessage
+            key={msg.id}
+            message={msg.message}
+            isOwnMessage={msg.isOwnMessage}
+            userName={msg.userName}
+            timestamp={msg.timestamp}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
