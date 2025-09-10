a29970b58a46cf91c4c108848c0a19edbc2404e5
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-09 19:12:26 -0600

    fix: adjust padding and spacing in ChatWindowView for improved layout consistency

--- src/sections/chat/view/chat-window-view.tsx
+++ src/sections/chat/view/chat-window-view.tsx
@@ -4,17 +4,17 @@ import { sampleMessages } from "./data/message.mock";
 
 export const ChatWindowView = () => {
   return (
-    <ScrollArea className="h-full p-4 bg-background">
-      <div className="space-y-2">
-        {sampleMessages.map((msg) => (
-          <ChatMessage
-            key={msg.id}
-            message={msg.message}
-            isOwnMessage={msg.isOwnMessage}
-            userName={msg.userName}
-            timestamp={msg.timestamp}
-          />
-        ))}
+    <ScrollArea className="h-full px-4 bg-background">
+      <div className="space-y-2 pt-4">
+      {sampleMessages.map((msg) => (
+        <ChatMessage
+        key={msg.id}
+        message={msg.message}
+        isOwnMessage={msg.isOwnMessage}
+        userName={msg.userName}
+        timestamp={msg.timestamp}
+        />
+      ))}
       </div>
     </ScrollArea>
   );
