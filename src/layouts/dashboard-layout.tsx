d54981b9dec4f6bc7e86a3fa24d86964194aeca0
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-15 15:08:10 -0600

    fix: reorder imports and refine layout styles in DashboardLayout component

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,7 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, useSidebar } from "@/components/ui/sidebar";
-import { Outlet } from "react-router";
 import clsx from "clsx";
+import { Outlet } from "react-router";
 
 export const DashboardLayout = () => {
   const { open } = useSidebar();
@@ -13,9 +13,13 @@ export const DashboardLayout = () => {
         <main className="flex flex-col gap-4 p-8 w-full h-screen bg-slate-100">
           <div
             className={clsx(
-              "bg-sidebar  h-full rounded-lg overflow-hidden flex flex-col w-full transition-all duration-300 mx-auto",
-              open ? "max-w-[calc(100vw-21.7rem)]" : "max-w-[calc(100vw-4rem)]"
+              "bg-sidebar h-full rounded-lg overflow-hidden flex flex-col w-full mx-auto",
+              "transition-transform duration-300 ease-in-out will-change-transform",
+              open ? "w-[calc(100vw-21.7rem)]" : "w-[calc(100vw-4rem)]"
             )}
+            style={{
+              transform: "translate3d(0, 0, 0)",
+            }}
           >
             <Outlet />
           </div>
