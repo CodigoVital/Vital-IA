f82937e1fbf1d1a2ceae1c41563280ac07a10683
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-15 11:08:46 -0600

    fix: add overflow-hidden class to SidebarInset for improved layout handling

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -9,11 +9,11 @@ export const DashboardLayout = () => {
   return (
     <>
       <AppSidebar />
-      <SidebarInset>
+      <SidebarInset className="overflow-hidden">
         <main className="flex flex-col gap-4 p-8 w-full h-screen bg-slate-100">
           <div
             className={clsx(
-              "bg-sidebar h-full rounded-lg overflow-hidden flex flex-col w-full transition-all duration-300 mx-auto",
+              "bg-sidebar  h-full rounded-lg overflow-hidden flex flex-col w-full transition-all duration-300 mx-auto",
               open ? "max-w-[calc(100vw-21.7rem)]" : "max-w-[calc(100vw-4rem)]"
             )}
           >
