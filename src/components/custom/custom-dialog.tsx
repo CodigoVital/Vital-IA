29d897696dede9928872c132ff473f084b9dda23
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-22 11:21:00 -0600

    feat: add iconName prop to CustomDialog and export LucideName type for improved icon usage

--- src/components/custom/custom-dialog.tsx
+++ src/components/custom/custom-dialog.tsx
@@ -1,22 +1,27 @@
 import { useIsMobile } from "@/hooks/use-mobile";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import LucideIcon, { type LucideName } from "../lucide-icon-name";
+
 
 interface CustomPopupProps {
   title: string;
   children: React.ReactNode;
   className?: string;
   ariaLabel?: string;
+  iconName?: LucideName;
 }
 const CustomDialog = ({
   title,
   children,
   className,
   ariaLabel,
+  iconName,
 }: CustomPopupProps) => {
   const isMobile = useIsMobile();
   return (
     <Dialog>
       <DialogTrigger aria-label={ariaLabel} className={className}>
+        <LucideIcon name={iconName} />
         {title}
       </DialogTrigger>
       <DialogContent
