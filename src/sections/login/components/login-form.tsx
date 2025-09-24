1901b69b603627db204244f68a36c186f93d93cf
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-24 09:47:20 -0600

    feat: update LoginForm component with localized labels and improved styling

--- src/sections/login/components/login-form.tsx
+++ src/sections/login/components/login-form.tsx
@@ -1,41 +1,59 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { Link } from "react-router";
 
 export const LoginForm = () => {
   return (
     <form>
       <div className="flex flex-col gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Label htmlFor="email" className="text-primary-custom-text">
+            Correo
+          </Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="m@example.com"
+            required
+            className={cn("rounded border-custom-border", `bg-white `)}
+          />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
+            <Label htmlFor="password" className="text-primary-custom-text">
+              Contraseña
+            </Label>
             <a
               href="#"
-              className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+              className="ml-auto inline-block text-sm underline-offset-4 hover:underline text-primary-custom-text"
             >
-              Forgot your password?
+              ¿Olvidaste tu contraseña?
             </a>
           </div>
-          <Input id="password" type="password" required />
+          <Input
+            id="password"
+            type="password"
+            required
+            className={cn("rounded border-custom-border", `bg-white `)}
+            placeholder="********"
+          />
         </div>
         <div className="flex flex-col gap-3">
           <Button type="submit" className="w-full">
             Login
           </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
-          </Button>
         </div>
       </div>
-      <div className="mt-4 text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Sign up
-        </a>
+      <div className="mt-4 text-center text-sm text-primary-custom-text ">
+        ¿No tienes una cuenta?{" "}
+        <Link
+          to="#"
+          className="underline underline-offset-4 text-primary-custom-text"
+        >
+          Regístrate
+        </Link>
       </div>
     </form>
   );
