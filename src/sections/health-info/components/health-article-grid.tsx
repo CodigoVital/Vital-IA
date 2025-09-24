2ca7fe40e88c18c17b18460e30285d45c8db2981
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-23 19:56:56 -0600

    fix: update HealthArticleGrid layout for responsive design

--- src/sections/health-info/components/health-article-grid.tsx
+++ src/sections/health-info/components/health-article-grid.tsx
@@ -10,11 +10,11 @@ const HealthArticleGrid = ({ info }: HealthArticleGridProps) => {
   return (
     <section className="mt-4 flex-1 min-h-0">
       <ScrollArea className="w-full h-full">
-        <article className="grid grid-cols-2 gap-4  h-full">
-          {info.map((c) => (
-            <HealthArticleCard key={c.id} info={c} />
-          ))}
-        </article>
+      <article className="grid grid-cols-1 sm:grid-cols-2 gap-4 h-full">
+        {info.map((c) => (
+        <HealthArticleCard key={c.id} info={c} />
+        ))}
+      </article>
       </ScrollArea>
     </section>
   );
