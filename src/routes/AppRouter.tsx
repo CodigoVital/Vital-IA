71822391f038bbec8027ff635920d39d786d709f
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-24 09:00:11 -0600

    feat: add LoginPage component and route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,12 +2,14 @@ import { DashboardLayout } from "@/layouts/dashboard-layout";
 import { ChatPage } from "@/Page/chat/chat-page";
 import DiagnosisPage from "@/Page/diagnosis/diagnosisPage";
 import { HealthInfoPage } from "@/Page/health-info/health-info-page";
+import LoginPage from "@/Page/login/login-page";
 import { Route, Routes } from "react-router";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
+        <Route index path="/login" element={<LoginPage />} />
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<ChatPage />} />
           <Route path="/chat" element={<ChatPage />} />
