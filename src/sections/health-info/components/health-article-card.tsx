dc0390593821f0aa94dc52f8d432c317bd318b50
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-23 18:07:31 -0600

    fix: improve image handling in HealthArticleCard component

--- src/sections/health-info/components/health-article-card.tsx
+++ src/sections/health-info/components/health-article-card.tsx
@@ -9,15 +9,20 @@ const HealthArticleCard = ({ info }: HealthArticleCardProps) => {
   return (
     <Card className="w-full h-full flex gap-0 p-0 flex-col bg-white border border-custom-border rounded-md">
       {info.img ? (
-        <img
-          className="w-full h-32 rounded-none object-cover"
-          src={info.img}
-          alt={info.title}
-        />
-      ) : (
-        <div className="w-full h-32 flex items-center justify-center bg-gray-200 text-gray-500">
-          Sin imagen disponible
+        <div className="relative w-full h-32">
+          <img
+            className="w-full h-32 rounded-none object-cover"
+            src={info.img}
+            alt={info.title}
+          />
+          <h1 className="absolute bottom-1 left-2 text-white font-semibold bg-black/50 px-2 py-1 rounded">
+            {info.title}
+          </h1>
         </div>
+      ) : (
+        <h1 className="w-full h-32 flex items-center justify-center bg-gray-200 text-gray-500">
+          {info.title}
+        </h1>
       )}
 
       <div className="flex flex-col justify-between flex-1 p-2">
