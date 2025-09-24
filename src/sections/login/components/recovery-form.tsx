7eed6178e11e1959bd2b0e66494c76c01dea4abe
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-24 11:26:15 -0600

    feat: enhance RecoveryForm with bold email label and improved styling for recovery message

--- src/sections/login/components/recovery-form.tsx
+++ src/sections/login/components/recovery-form.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
@@ -7,7 +8,7 @@ export const RecoveryForm = () => {
     <form>
       <div className="flex flex-col gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email" className="text-primary-custom-text">
+          <Label htmlFor="email" className="text-primary-custom-text font-bold">
             Correo
           </Label>
           <Input
@@ -17,10 +18,20 @@ export const RecoveryForm = () => {
             className={cn("rounded border-custom-border", `bg-white `)}
           />
         </div>
-        <p className="text-sm text-primary-custom-text">
+        <p className="text-[10px] sm:text-xs text-center text-primary-custom-text  overflow-hidden text-ellipsis">
           Si este correo está registrado, entonces enviaremos un método de
           recuperación
         </p>
+
+        <Button
+          type="submit"
+          className={cn(
+            "bg-primary-custom font-light hover:bg-primary-custom/80",
+            `w-full`
+          )}
+        >
+          Iniciar sesión
+        </Button>
       </div>
     </form>
   );
