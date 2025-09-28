8c4947d2a01c23ceb45b27e15c59f95cb66f3cc5
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-28 13:13:40 -0600

    fix: Refactor SymptomCommonList to use ScrollArea for improved layout and scrolling

--- src/sections/diagnosis/components/symptom-common-list.tsx
+++ src/sections/diagnosis/components/symptom-common-list.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { sintomasComunes } from "../data/symptom.mock";
 import { cn } from "@/lib/utils";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 interface SymptomCommonListProps {
   sintomasSeleccionados: string[];
@@ -12,29 +13,36 @@ export const SymptomCommonList = ({
   seleccionarSintomaComun,
 }: SymptomCommonListProps) => {
   return (
-    <section id="common-symptom" className="flex   flex-col gap-2" aria-label="Síntomas comunes">
+    <section
+      id="common-symptom"
+      className="flex   flex-col gap-2"
+      aria-label="Síntomas comunes"
+    >
       <h3 className="text-[#1A2E46] text-base font-bold mb-1">
         Síntomas Comunes
       </h3>
-      <div className="flex flex-wrap gap-2">
-        {sintomasComunes.map((sintoma) => (
-          <Button
-            key={sintoma}
-            className={cn(
-              `px-4 py-2 rounded-xl border border-[#E0E0E0] bg-[#F5F7FA] text-[#1A2E46] text-base font-medium shadow-sm transition-colors duration-150 ${
-                sintomasSeleccionados.includes(sintoma)
-                  ? "bg-[#2DC6C4] text-white border-[#2DC6C4]"
-                  : "hover:bg-[#E6F7F6] hover:text-[#1A2E46] focus:text-[#1A2E46]"
-              }`
-            )}
-            onClick={() => seleccionarSintomaComun(sintoma)}
-            type="button"
-            aria-label={`Seleccionar síntoma ${sintoma}`}
-          >
-            {sintoma}
-          </Button>
-        ))}
-      </div>
+      <ScrollArea className="w-full overflow-x-auto rounded-md ">
+        <div className="flex gap-2 flex-nowrap p-2">
+          {sintomasComunes.map((sintoma) => (
+            <Button
+              key={sintoma}
+              className={cn(
+                `px-4 py-2 rounded-xl border border-[#E0E0E0] bg-[#F5F7FA] text-[#1A2E46] text-base font-medium shadow-sm transition-colors duration-150 ${
+                  sintomasSeleccionados.includes(sintoma)
+                    ? "bg-[#2DC6C4] text-white border-[#2DC6C4]"
+                    : "hover:bg-[#E6F7F6] hover:text-[#1A2E46] focus:text-[#1A2E46]"
+                }`
+              )}
+              onClick={() => seleccionarSintomaComun(sintoma)}
+              type="button"
+              aria-label={`Seleccionar síntoma ${sintoma}`}
+            >
+              {sintoma}
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </section>
   );
 };
