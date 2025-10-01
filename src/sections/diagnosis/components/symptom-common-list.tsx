6f0f6b1422da4fa46a9c82e85a0a3a462c8b116c
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-30 19:27:26 -0600

    feat: Update SymptomCommonList and SymptomInstructions components for improved styling consistency

--- src/sections/diagnosis/components/symptom-common-list.tsx
+++ src/sections/diagnosis/components/symptom-common-list.tsx
@@ -18,17 +18,17 @@ export const SymptomCommonList = ({
       className="flex   flex-col gap-2"
       aria-label="Síntomas comunes"
     >
-      <h3 className="text-[#1A2E46] text-base font-bold mb-1">
+      <h3 className="text-primary-custom-text font-latotext-base font-bold mb-1">
         Síntomas Comunes
       </h3>
       <ScrollArea className="w-full overflow-x-auto rounded-md ">
         <div className="flex gap-2 flex-nowrap p-2">
           {sintomasComunes.map((sintoma) => (
             <Button
-            id="add-custom-symptom"
+              id="add-custom-symptom"
               key={sintoma}
               className={cn(
-                `px-4 py-2 rounded-xl border border-[#E0E0E0] bg-[#F5F7FA] text-[#1A2E46] text-base font-medium shadow-sm transition-colors duration-150 ${
+                `px-4 py-2 rounded-xl border-2 border-[#E0E0E0] bg-[#F5F7FA] text-primary-custom-text font-lato  font-medium transition-colors duration-150 ${
                   sintomasSeleccionados.includes(sintoma)
                     ? "bg-[#2DC6C4] text-white border-[#2DC6C4]"
                     : "hover:bg-[#E6F7F6] hover:text-[#1A2E46] focus:text-[#1A2E46]"
