5379df5281ea562b3c714e965ce61c2ff567f307
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-30 18:14:19 -0600

    feat: Add mobile responsiveness to AuthHeader component

--- src/components/custom/auth-header.tsx
+++ src/components/custom/auth-header.tsx
@@ -1,6 +1,9 @@
+import { useIsMobile } from "@/hooks/use-mobile";
 import Logo from "../logo";
 
 const AuthHeader = () => {
+  const isMobile = useIsMobile();
+  if (isMobile) return null;
   return (
     <header className="bg-transparent  fixed ">
       <div className="flex items-center p-4 ">
