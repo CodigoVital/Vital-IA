0edec7978b40680533089b36800ccfc37e2ab82f
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-01 16:00:27 -0600

    feat: Define ChatRequest and ChatResponse interfaces for chat API integration

--- src/store/services/chat/chatApi.ts
+++ src/store/services/chat/chatApi.ts
@@ -1,15 +1,20 @@
 import { API_URL } from "@/constants";
+import type {
+  ChatRequest,
+  ChatResponse,
+} from "@/types/chatbot/chatbot-response";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 export const chatApi = createApi({
   reducerPath: "chatApi",
   tagTypes: ["Chat"],
   baseQuery: fetchBaseQuery({ baseUrl: `${API_URL}` }),
   endpoints: (builder) => ({
-    getMessages: builder.query({
+    getMessages: builder.query<ChatResponse[], void>({
       query: () => ``,
       providesTags: ["Chat"],
     }),
-    sendMessage: builder.mutation({
+    sendMessage: builder.mutation<ChatResponse, ChatRequest>({
       query: (newMessage) => ({
         url: "/webhook/chatbot",
         method: "POST",
