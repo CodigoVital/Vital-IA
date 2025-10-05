e5a177f4a8c5108c89f051b894f312eb8b7995d8
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-05 10:33:20 -0600

    feat: add navigation for unauthenticated users in ProtectedRoutes component

--- src/layouts/auth/protected-routes.tsx
+++ src/layouts/auth/protected-routes.tsx
@@ -1,10 +1,14 @@
 import { useAppSelector } from "@/hooks/use-selector";
 import type { PropsWithChildren } from "react";
+import { Navigate } from "react-router";
 
 const ProtectedRoutes = ({ children }: PropsWithChildren) => {
   const isAuthenticated = useAppSelector(
     (state) => state.authSlice.isAuthenticated
   );
+  if (!isAuthenticated) {
+    return <Navigate to="/auth" replace />;
+  }
 
   return <>{children}</>;
 };
