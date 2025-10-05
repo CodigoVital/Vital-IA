4c101619249a3d5c0f799903fef610259903d8e6
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-05 10:36:22 -0600

    feat: update AppRouter to streamline route definitions and enhance ProtectedRoutes integration

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -4,17 +4,27 @@ import DashboardRoutes from "./sections/dashboard";
 import RegisterPage from "@/Page/auth/register-page";
 import RecoveryPage from "@/Page/auth/recovery-page";
 import AuthLayout from "@/layouts/auth/auth-layout";
+import ProtectedRoutes from "@/layouts/auth/protected-routes";
+import { DashboardLayout } from "@/layouts/dashboard/dashboard-layout";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="/auth/*" element={<AuthLayout />}>
-          <Route index element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="recovery" element={<RecoveryPage />} />
+        <Route path="/auth/*" element={<AuthLayout />} />
+        <Route index element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="recovery" element={<RecoveryPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoutes>
+              <DashboardLayout />
+            </ProtectedRoutes>
+          }
+        >
+          <Route path="/*" element={<DashboardRoutes />} />
         </Route>
-        <Route path="/*" element={<DashboardRoutes />} />
       </Routes>
     </>
   );
