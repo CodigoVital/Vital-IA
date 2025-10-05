a5254e219b32239bf03d69b46aa0f4c2d9669048
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-05 13:24:20 -0600

    feat: enhance AuthLayout to conditionally apply mobile styles based on screen size

--- src/layouts/auth/auth-layout.tsx
+++ src/layouts/auth/auth-layout.tsx
@@ -1,9 +1,15 @@
 import AuthHeader from "@/components/custom/auth-header";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { Outlet } from "react-router";
 
 const AuthLayout = () => {
+  const isMobile = useIsMobile();
   return (
-    <div className="min-h-screen flex flex-col w-full">
+    <div
+      className={`flex flex-col w-full min-h-screen ${
+        isMobile ? "h-screen overflow-hidden" : ""
+      }`}
+    >
       <AuthHeader />
       <main className="flex-1 flex items-center justify-center w-full">
         <Outlet />
